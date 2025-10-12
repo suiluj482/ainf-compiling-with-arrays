17d66da2a2c2fec003ed27afd46f7932e786ba70
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-10-12 03:40:48 +0200

    multilayerPerceptron

--- src/Polara/Tests/Examples/MultilayerPerceptron.lean
+++ src/Polara/Tests/Examples/MultilayerPerceptron.lean
@@ -9,50 +9,60 @@ notation:max ty"[["n"]]" => Ty.array n ty
 notation:max ty"[["n","m"]]" => Ty.matrix n m ty
 
 -- if' makes no norm faster norm slow, maxf makes no norm impossible
-def Tm.relu (x: Term flt): Term flt := if' x <' tlitf 0 then tlitf 0 else x
--- def Tm.relu (x: Term flt): Term flt := x.maxf (tlitf 0)
+-- def Tm.relu (x: Term flt): Term flt := if' x <' tlitf 0 then tlitf 0 else x
+def Tm.relu (x: Term flt): Term flt := x.maxf (tlitf 0)
+
+def loss {m: Nat} :=
+  fun' y:flt[[m]] => fun' eY:flt[[m]] =>
+    let' diff := (y - eY); diff * diff
+    |>.toVPar
+
+#eval loss (m:=10) |>.normVPar
 
 def neuron {n: Nat} :=
   fun' x: flt[[n]] => fun' b: flt => fun' w: flt[[n]] =>
-    (for' i => x[[i]] * w[[i]]).sumf + b |>.relu
+    (x * w).sumf + b |>.relu
+
+#eval neuron (n:=10) |>.normVPar
 
 def layer {n m: Nat} :=
  fun' x: flt[[n]] => fun' w: flt[[m]] ×× flt[[m,n]] =>
   for' i => (neuron @@ x @@ w.fst[[i]] @@ w.snd[[i]])
 
-def layerLearnStep :=
-  fun' x: flt[[20]] => fun' y: flt[[5]] =>
-    fun' w: flt[[5]] ×× flt[[5, 20]] =>
-      let' md :=(@layer 20 5 @@ x).dr.le;
-      let't _, d := md @@ w;
-      d @@ y
+#eval (@layer 10 5) |>.normVPar
 
-def multilayerPerceptron :=
-  fun' x =>
-    fun' ws: (flt[[10]] ×× flt[[10, 20]]) ×× (flt[[5]] ×× flt[[5, 10]]) =>
-      let't w, ws := ws;
-      let' x := layer @@ x @@ w;
-      let' w := ws;
-      let' x := layer @@ x @@ w;
+-- def layerLearnStep :=
+--   fun' x: flt[[20]] => fun' y: flt[[5]] =>
+--     fun' w: flt[[5]] ×× flt[[5, 20]] =>
+--       let' md :=(@layer 20 5 @@ x).dr.le;
+--       let't _, d := md @@ w;
+--       d @@ y
+
+
+def Weights (n k m: Nat) := (flt[[k]] ×× flt[[k, n]]) ×× (flt[[m]] ×× flt[[m, k]])
+
+def multilayerPerceptron {n k m: Nat} :=
+  fun' x:flt[[n]] =>
+    fun' ws: Weights n k m =>
+      let x := layer @@ x @@ ws.fst;
+      let x := layer @@ x @@ ws.snd;
       x
 
-def learnStep :=
-  fun' x: flt[[20]] => fun' y: flt[[5]] =>
-    fun' ws: (flt[[10]] ×× flt[[10, 20]]) ×× (flt[[5]] ×× flt[[5, 10]]) =>
+#eval @multilayerPerceptron 10 5 1 |>.normVPar
+
+def learnStep {n k m: Nat} :=
+  fun' x: flt[[n]] => fun' y: flt[[m]] =>
+    fun' ws: Weights n k m =>
       (
-        (multilayerPerceptron.dr.le.normVPar @@ x).fst @@ ws
+        (fun' ws => loss @@ (multilayerPerceptron @@ x @@ ws) @@ y).normVPar.dr.le.normVPar @@ ws
       ).snd @@ y
 
-#eval multilayerPerceptron
-#eval multilayerPerceptron.dr.le --.normVPar
-#eval learnStep @@ (for' x => tlitf 0) @@ (for' y => tlitf 1) @@ (Tm.zero _) |>.toVPar
+#eval @multilayerPerceptron 10 5 1 |>.normVPar.dr.le.normVPar
+#eval (@learnStep 5 2 1) @@ (for' x => tlitf 0) @@ (for' y => tlitf 1) @@ (Tm.zero _) |>.toVPar
 
+def x := (for' x:10 => tlitf 0) |>.toVPar
+def y := (for' y:1 => tlitf 1) |>.toVPar
 
--- def learnStep :=
---   fun' x: flt[[20]] => fun' y: flt[[5]] =>
---     fun' ws: (flt[[10]] ×× flt[[10, 20]]) ×× (flt[[5]] ×× flt[[5, 10]]) =>
---       (
---         (multilayerPerceptron @@ x).dr.le.normVPar @@ ws -- the x comes from the outside, which is forbidden
---       ).snd @@ y
-
-#eval (fun'v x:flt => Tm.var (x.changeType: VPar nat)).normVPar -- normVPar checks types
+#eval fun' ws: Weights 10 5 1 =>
+  (fun' ws:Weights 10 5 1 => loss @@ (multilayerPerceptron @@ x @@ ws) @@ y).normVPar.dr.le.normVPar
+  @@ ws -- |>.normVPar
